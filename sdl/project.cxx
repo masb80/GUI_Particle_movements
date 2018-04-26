0376e000b89216da1c92ed67108f3d3c2906be3b
masb80 <masb80@mun.ca>
2018-04-26 13:58:24 -0230

    background colour change test

--- sdl/project.cxx
+++ sdl/project.cxx
@@ -115,11 +115,12 @@ int main()
 		unsigned char green =(unsigned char)((1 + sin(elapsed*0.001))*128); // multiply with 128 to make 255. becuse green is highest 255. 
 		unsigned char red =(unsigned char)((1 + sin(elapsed*0.002))*128); // multiply with 128 to make 255. becuse green is highest 255. 
 		unsigned char blue =(unsigned char)((1 + sin(elapsed*0.003))*128); // multiply with 128 to make 255. becuse green is highest 255. 
+		
 		for(int y =0; y < Screen::SCREEN_HEIGHT;y++)
 		{
 			for(int x = 0; x < Screen::SCREEN_WIDTH; x++)
 			{
-				screen.setPixel(x,y,red,green,blue);
+				screen.setPixel(x,y,0,green,0);
 			}	
 		}	
 		*/
@@ -133,7 +134,7 @@ int main()
 			//
 		}
 		*/
-//		screen.setPixel(400,300,255,255,255);
+	//	screen.setPixel(400,300,0,255,0);
 		screen.update();
 		if(screen.processEvents() == false)
 		{
