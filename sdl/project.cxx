576123e96d8369ed955311fecb6d3aa49b4f3eb5
masb80 <masb80@mun.ca>
2018-05-04 10:39:22 -0230

    nicely rainbow without screen clean

--- sdl/project.cxx
+++ sdl/project.cxx
@@ -98,7 +98,7 @@ int main()
 		// updates particles
 		// draw praticles
 		int elapsed = SDL_GetTicks(); // This is a number in SDL in mili second
-		screen.clear();
+//		screen.clear();
 		swarm.update( elapsed); // every mili second it will updates the position of the particles(giving constant speed as a position change in in the particles and calling from the swarm, )
 		unsigned char green =(unsigned char)((1 + sin(elapsed*0.001))*128); // multiply with 128 to make 255. becuse green is highest 255. 
 		unsigned char red =(unsigned char)((1 + sin(elapsed*0.002))*128); // multiply with 128 to make 255. becuse green is highest 255. 
